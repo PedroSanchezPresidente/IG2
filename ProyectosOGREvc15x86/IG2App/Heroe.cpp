3fa2c5e8087a3b52926249cd471ca849cf6119a6
Marc0sPM <marcospm0306@gmail.com>
2024-10-02 17:01:21 +0200

    el ogro rota

--- ProyectosOGREvc15x86/IG2App/Heroe.cpp
+++ ProyectosOGREvc15x86/IG2App/Heroe.cpp
@@ -3,20 +3,22 @@
 bool Heroe::keyPressed(const OgreBites::KeyboardEvent& evt) {
     
     if (evt.keysym.sym == SDLK_UP || evt.keysym.sym == SDLK_w) {
-        //mSnowmanNode->rotate(Ogre::Quaternion(0.0, 0.0, 180.0, 0.0));
+        cout << mNode->getOrientation();
+
+        mNode->setOrientation(1, 0, Radian(180).valueRadians(), 0);
 
         move(Ogre::Vector3(0.0, 0.0, -10.0));
     }
     else if (evt.keysym.sym == SDLK_DOWN ||evt.keysym.sym == SDLK_s) {
-        //mSnowmanNode->rotate(Ogre::Quaternion(0.0, 0.0, 180.0, 0.0));
+        mNode->setOrientation(1, 0, 0, 0);
         move(Ogre::Vector3(0.0, 0.0, 10.0));
     }
     else if (evt.keysym.sym == SDLK_LEFT || evt.keysym.sym == SDLK_a) {
-        //mSnowmanNode->rotate(Ogre::Quaternion(0.0, 0.0, 180.0, 0.0));
+        mNode->setOrientation(1, 0,-1, 0); 
         move(Ogre::Vector3(-10.0, 0.0, 0.0));
     }
     else if (evt.keysym.sym == SDLK_RIGHT || evt.keysym.sym == SDLK_d) {
-        //mSnowmanNode->rotate(Ogre::Quaternion(0.0, 0.0, 180.0, 0.0));
+        mNode->setOrientation(1, 0, 1, 0); 
         move(Ogre::Vector3(10.0, 0.0, 0.0));
     }
 
