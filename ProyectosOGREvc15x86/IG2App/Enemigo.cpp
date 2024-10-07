15f4d87bc620e65e2c1e16c92f560b25e9a9142c
Marc0sPM <marcospm0306@gmail.com>
2024-10-07 16:53:05 +0200

    creacion clase enemigo
    sin implementacion, me daba pereza xdd

---
+++ ProyectosOGREvc15x86/IG2App/Enemigo.cpp
@@ -0,0 +1,8 @@
+#include "Enemigo.h"
+
+void Enemigo::frameRendered(const Ogre::FrameEvent& evt)
+{
+	// Evaluar las direcciones posibles  y hacer que se mueva
+	// Lo hacemos en clase porque me da pereza ahora mismo
+
+}
---
+++ ProyectosOGREvc15x86/IG2App/Enemigo.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "IG2Object.h"
+
+class Labyrinth;
+
+class Enemigo : public IG2Object
+{
+protected:
+	Labyrinth* lab;
+	int tileWidth;
+	int distance;
+	Vector3 dir = Vector3(0, 0, 0);
+	Vector3 newDir = Vector3(0, 0, 0);
+	Vector2 actualPos;
+	int speed = 10;
+
+	virtual void frameRendered(const Ogre::FrameEvent& evt);
+
+public:
+	Enemigo(Vector2 initPos, SceneNode* node, SceneManager* mSM, String mesh, Labyrinth* Lab, int TileWidth) : IG2Object(Vector3(initPos.y* TileWidth, 0, initPos.x* TileWidth), node, mSM, mesh), lab(Lab), tileWidth(TileWidth), actualPos(initPos), distance(0) {
+		// Set scale of Sinbad
+		setScale(Vector3(10, 10, 10));
+	}
+
+};
+
