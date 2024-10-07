24de55d272032d0376e379160ab551dd36da4c46
Marc0sPM <marcospm0306@gmail.com>
2024-10-07 16:54:14 +0200

    que soy bobo ;P

--- ProyectosOGREvc15x86/IG2App/Enemigo.cpp
+++ ProyectosOGREvc15x86/IG2App/Enemigo.cpp
@@ -3,6 +3,6 @@
 void Enemigo::frameRendered(const Ogre::FrameEvent& evt)
 {
 	// Evaluar las direcciones posibles  y hacer que se mueva
-	// Lo hacemos en clase porque me da pereza ahora mismo
+	// Lo hacemos en clase porque me da pereza ahora mismo	
 
 }
