0318c7f43bf435f342b440c2d0dd5adb8a975a39
anonymous <marcospm0306@gmail.com>
2024-12-04 14:09:20 +0300

    arreglo shader sky

--- ProyectosOGREvc15x86/media/IG2App/SpaceSkyZoomLightVS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/SpaceSkyZoomLightVS.glsl
@@ -4,15 +4,19 @@ in vec4 vertex;
 in vec2 uv0;     
 
 uniform mat4 modelViewProjMat;
-uniform float sintime;
+uniform float sintime; // [-1.0, 1.0]
+
+// Parametros para que zoom [0.3, 1.0]
+const float a = (1.0 - 0.3) / (1.0 - (-1.0)); 
+const float b = 0.3 - a * (-1.0); 
 
 out vec2 vUv0; 
 out float zf;   // Valor de zoom
 out float tiempo; 
 
 void main() {
-    // Calcular valor de zoom basado en sintime [0.75, 1.25]
-    float ZF = sintime * 0.25 + 1.0;
+    // Calcular valor zoom basado en sintime
+    float ZF = sintime * a + b;
 
     // Modificar coordenadas de textura para aplicar zoom
     vec2 uvCentered = uv0 - vec2(0.5, 0.5); 
